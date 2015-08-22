ee38d8a3457163419d0daa7164633f0328ba5a43
pragyandas <pragyandas@msn.com>
2015-08-23 02:00:33 +0530

    added gitignore

--- dist/griffin.js
+++ dist/griffin.js
@@ -1,10 +1,22 @@
 var griffin;
 (function (griffin) {
+    var rotation;
+    (function (rotation) {
+        rotation[rotation["auto"] = 0] = "auto";
+        rotation[rotation["vertical"] = 90] = "vertical";
+        rotation[rotation["horizontal"] = 0] = "horizontal";
+        rotation[rotation["inclined"] = 45] = "inclined";
+    })(rotation || (rotation = {}));
+})(griffin || (griffin = {}));
+/// <reference path="../interface/IChartOptions.ts" />
+/// <reference path="../interface/IChartData.ts" />
+var griffin;
+(function (griffin) {
     var BaseChart = (function () {
         function BaseChart(containerId) {
             this.containerId = containerId;
         }
-        BaseChart.prototype.render = function () {
+        BaseChart.prototype.render = function (chartData) {
             console.log('rendering chart');
         };
         BaseChart.prototype.setOptions = function (chartOptions) {
